6559a760b93a0cafadc1a88b77802df2b4eeb718
Christoph Stock <stockulus@icloud.com>
2019-04-09 13:14:17 +0200

    first server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hallo')
+import * as fastify from 'fastify'
+
+const server = fastify()
+
+server.get('/', {}, (_, reply) => {
+  reply.send('Hallo')
+})
+
+server.listen(3000, () => console.log('Server is running'))
