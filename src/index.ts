a3a8828d82841f79672c7fb8990e3fb01b79c6fb
Christoph Stock <stockulus@icloud.com>
2019-04-09 13:45:15 +0200

    basic pop / push

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,44 @@
 import * as fastify from 'fastify'
 
+const stack = []
+
 const server = fastify()
 
 server.get('/', {}, (_, reply) => {
   reply.send('Hallo')
 })
+
+server.get('/world', {}, (_, reply) => {
+  reply.send({ hello: 'world' })
+})
+
+server.post('/pop', {}, (_, reply) => {
+  if (stack.length <= 0) {
+    reply.send({ empty: true })
+  }
+  const x = stack.pop()
+  console.log('Pop', stack)
+  reply.send({ x })
+})
+
+server.post(
+  '/push',
+  {
+    schema: {
+      body: {
+        type: 'object',
+        properties: {
+          x: { type: 'number' }
+        },
+        required: ['x']
+      }
+    }
+  },
+  (request, reply) => {
+    stack.push(request.body.x)
+    console.log('Push', stack)
+    reply.send({ ok: true })
+  }
+)
 
 server.listen(3000, () => console.log('Server is running'))
