f4967b6895dbd4841af1f1e911295b5102d00dda
Christoph Stock <stockulus@icloud.com>
2019-04-10 08:48:29 +0200

    fixed typos

--- node_powershell/index.js
+++ node_powershell/index.js
@@ -1,18 +1,17 @@
-const Shell = require("node-powershell");
+const Shell = require('node-powershell')
 
 const ps = new Shell({
-  executionPolicy: "Bypass",
+  executionPolicy: 'Bypass',
   noProfile: true
-});
+})
 
-ps.addCommand("Get-ChildItem | convertto-json");
+ps.addCommand('Get-ChildItem | ConvertTo-json')
 ps.invoke()
   .then(output => {
-    const parsed = JSON.parse(output);
-    const string = JSON.stringify(parsed);
-    // const files = output.split("\n");
-    console.log(parsed, string);
+    const parsed = JSON.parse(output)
+    const string = JSON.stringify(parsed)
+    console.log(parsed, string)
   })
   .catch(err => {
-    console.log(err);
-  });
+    console.log(err)
+  })
